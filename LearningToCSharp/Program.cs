6e88ce02719963229800d4fff87654548a571ce8
x <x@WIN-R7POB5K0F89>
2021-11-04 21:26:01 +0300

    Добавьте файлы проекта.

---
+++ LearningToCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LearningToCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1 , -2 , 3 , -5 , -3 , 2 , 5 , -1 , 2 , -2};
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int AtI = numbers[i];
+                    int AtJ = numbers[j];
+
+                    if(AtI + AtJ == 0)
+                    {
+                        Console.WriteLine($" Pair ({AtI};{AtJ}) and his index ({i};{j})");
+                    }
+                }
+            }
+        }
+    }
+}
